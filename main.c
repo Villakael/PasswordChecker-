4b2fa46f1cd565c72e17e778337d809b3bfa076e
Kael Villa <villakael0@gmail.com>
2025-07-02 17:36:04 -0400

    Created the struct for the linked list and layed out the format for the menu

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h> 
 
+typedef struct Node {
+    int pswd; 
+    struct Node * next; 
+}
+
 int main(void) 
 {
-    printf("hello there");
+    int choice, id; 
+    
+    switch (choice) {
+        case 1: //password checker
+        case 2: //List previous passwords 
+        case 3: //Exit 
+    }
 
     return 0; 
 }
